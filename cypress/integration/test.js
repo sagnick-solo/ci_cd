3ada17200e851c5912d33bac1ce1e15cf0343b9b
Sagnick <sagnick.biswas@kreeti.com>
2021-12-22 22:29:06 +0530

    testing_ci

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,13 +1,8 @@
 /// <reference types="cypress" />  
 
 it('switchboard-sales', () => {
-    cy.visit('https://staging.switchboardsales.com/')
-
-    cy.get('#user_email')
-    .type('ashmita.sur+retaileradmin@kreeti.com')
-
-    cy.get('#user_password')
-    .type('kreeti123')
-
+    cy.visit("/")
+    cy.get('#user_email').type('ashmita.sur+retaileradmin@kreeti.com')
+    cy.get('#user_password').type('kreeti123')
     cy.get('.btn').click()
 })
