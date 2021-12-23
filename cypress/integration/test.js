e2d7bdeea1f9c9eabf16d243a378e034217648a6
Sagnick <sagnick.biswas@kreeti.com>
2021-12-23 15:42:48 +0530

    ss

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,8 +1,21 @@
 /// <reference types="cypress" />  
+import faker from 'faker'
 
-it('switchboard-sales', () => {
-    cy.visit("/")
-    cy.get('#user_email').type('ashmita.sur+retaileradmin@kreeti.com')
-    cy.get('#user_password').type('kreeti123')
-    cy.get('.btn').click()
+describe('pesitive-negative', () => {
+
+    it('switchboard-sales', () => {
+        cy.visit("/")
+        cy.get('#user_email').type('ashmita.sur+retaileradmin@kreeti.com')
+        cy.get('#user_password').type('kreeti123')
+        cy.get('.btn').click()
+        cy.location('pathname').should('contain', '/retailer/profiles')
+    })
+
+    it('switchboard-sales', () => {
+        cy.visit("/")
+        cy.get('#user_email').type(faker.internet.email())
+        cy.get('#user_password').type(faker.internet.password())
+        cy.get('.btn').click()
+        cy.location('pathname').should('contain', '/retailer/profiles')
+    })
 })
